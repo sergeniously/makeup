49fe7a63d181ce532962bc3384624edb73c75a84
Sergey Belenkov <sergeniously@mail.ru>
2022-10-28 12:01:01 +0500

    boost module: provide variable to store not found libraries

--- makeup/Boost.mk
+++ makeup/Boost.mk
@@ -1,11 +1,17 @@
 #####################
 # Boost makeup module
 #  implements macros to find boost includes and libraries
+# Provide variables:
+#  BOOST_FOUND=yes|no: whether boost location is found or not
+#  BOOST_INCLUDE_DIR: boost include directory if found, empty otherwise
+#  BOOST_LIBRARY_DIR: boost libraries directory if found, empty otherwise
+#  BOOST_LIBRARIES: found boost libraries with boost prefix, ready to be linked
+#  BOOST_NOT_FOUND: not found libraries without boost prefix
 
 # Macro to find boost location and define directory variables
 # Options:
 #   PATH:path: additional root path to search boost libraries
-#   LIBS:list: specific libraries to search and store to BOOST_LIBRARIES variable
+#   LIBS:list: specific libraries to search and store at BOOST_LIBRARIES variable
 # Example:
 #   $(call find_boost,PATH:$(HOME)/boost LIBS:system;regex)
 define find_boost # ([PATH:path ...] [LIBS:list])
@@ -20,10 +26,11 @@ BOOST_LIBRARY_DIR=\
 		$(wildcard $(call opt_each,$1,PATH:%,%/lib* %/stage/lib*) \
 			/opt/local/lib* /usr/local/lib* /opt/lib* /usr/lib* /lib* /opt/homebrew/lib* C:/Boost/lib),\
 		$(wildcard $(path)/libboost_system*)))))
+BOOST_NOT_FOUND=
 )$(eval \
 BOOST_FOUND=$(if $(and $(BOOST_INCLUDE_DIR),$(BOOST_LIBRARY_DIR)),yes,no)
 BOOST_LIBRARIES=\
-	$(if $(BOOST_LIBRARY_DIR),$(foreach lib,$(call opt_list,$1,LIBS:%),\
-		$(if $(wildcard $(BOOST_LIBRARY_DIR)/libboost_$(lib)*),boost_$(lib))))
+	$(if $(BOOST_LIBRARY_DIR),$(foreach lib,$(sort $(call opt_list,$1,LIBS:%)),\
+		$(if $(wildcard $(BOOST_LIBRARY_DIR)/libboost_$(lib)*),boost_$(lib),$(eval BOOST_NOT_FOUND+=$(lib)))))
 )
 endef
